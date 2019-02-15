370ccc4c302dbe0eef896b1e8a42bb658ae42e72
uemurakaito <kituemr@gmail.com>
2019-02-15 10:46:43 +0900

    scrape the title

--- cheerio.js
+++ cheerio.js
@@ -1,13 +1,22 @@
 const client = require('cheerio-httpcli');
 
-client.fetch('https://tv.so-net.ne.jp/chart/40.action', (err, $, res) => {
-    const result = [];
-    $('.normal_list').each(function (id, el) {
-        const month = $(this).find('.normal_h2').text();
 
-        $(this).find('li').each(function (id, el) {
-            result.push(month + $(this).text().replace(/\r?\n?/g, ''));
-        });
+//コールバック関数は非同期処理の際にこの処理だけはこの順番でやってほしいというのを記述するためにできた？
+
+client.fetch('https://tv.so-net.ne.jp/chart/40.action', (err, $, res,body) => {
+    const result = [];
+    console.log(res.headers);
+    console.log($("title").text());
+    //.find('span .schedule-summary')
+    $("#chartColumn[class ^= 'cell-genre']").each(function (id, el) {
+        console.log(el);
+        console.log($(this).text());
+        if ($(this).hasClass('cell-schedule cell-genre*')) {
+            $(this).text('True');
+            console.log("tafderqgerq");
+          } else {
+            $(this).text('False');
+          }
     });
 
     console.log(result.join('\n'));
